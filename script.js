3e3c653d2965d0d946d9fbc48d0cd8af1d84e53b
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-04 15:29:16 +0300

    добавлено задание для переменной screens

--- script.js
+++ script.js
@@ -11,40 +11,9 @@ console.log("adaptive", adaptive);
 console.log(screens.length);
 console.log("Стоимость верстки экранов " + screenPrice + " рублей");
 console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(","));
 console.log(
     "Процент отката посреднику за работу " +
     fullPrice * (rollback / 100) +
     " рублей"
 );
-
-// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
-// Вывести в консоль длину строки screens
-// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
-// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-
-// typeof myVar
-//bigint +n-только целочисленных
-//операторы унарные(+/-5,++) и бинарные (+,-,*,/,%,** - возведение в степень)
-//логические <,>, >=,<=, == (может быть использован для сравнения разных типов данных), !=, ===, !==(строго для сравнения данных одного типа)
-//Math.floor округление в меньшую
-//Math.ceil округление в большую
-//Math.round округление до ближайшего
-//Math.trunc удаление дробной части
-//Math.random случайное числ от 0 до 1
-//Math.max(3,5)
-//Math.sqrt(25)
-//Math.pow(5,2)
-
-//str.length
-//str.toUpperCase(), toLowercase()
-//str.charAt(2),str[2]
-//str.substring(6)
-//str.substring(6,15)
-//str.slice(-6)
-//str.slice(-6)
-//str.substr(0,2), где 2 - кол-во символов, кот нужно отрезать
-//str.indexOf("my")
-//str.includes("my")
-//str.replace
-//str.split(" ")
