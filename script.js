a071eeaf20c2e1791e359f86c1a34bf5bf502b2d
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-09 23:33:32 +0300

    домашнее задание к уроку 4

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 let title = prompt("Как называется ваш проект?");
-let elem = prompt("Какие типы экранов нужно разработать?");
-var screens = [];
-screens.push(elem);
-console.log(screens);
+let newTitle = "";
+let screens = prompt("Какие типы экранов нужно разработать?");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
-
 let fullPrice = 0.0;
 let rollback = (5 * fullPrice) / 100;
 let adaptive = prompt("Нужен ли адаптив на сайте?");
-
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = prompt("Сколько это будет стоить?");
-
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = prompt("Сколько это будет стоить?");
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(fullPrice - rollback);
-console.log(servicePercentPrice);
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
 switch (fullPrice) {
     case fullPrice > 30000:
         console.log("Даем скидку в 10%");
@@ -32,22 +27,33 @@ switch (fullPrice) {
         break;
 }
 
-console.log("title", title);
-console.log("fullPrice", fullPrice);
-console.log("adaptive", Boolean(adaptive));
-console.log(screens.length);
-console.log(
-    "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани"
-);
-console.log(
-    "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани"
-);
-//console.log(screens.toLowerCase().split(","));
-console.log(
-    "Процент отката посреднику за работу " +
-    fullPrice * (rollback / 100) +
-    " рублей"
-);
+function getFullPrice(screenPrice, allServicePrices) {
+    fullPrice = screenPrice + allServicePrices;
+    return fullPrice;
+}
+
+function getTitle(title) {
+    newTitle = title.trim().charAt(0).toUpperCase();
+    return newTitle;
+}
+
+function getRollbackMessage() {
+    return rollback;
+}
+
+const allServicePrices = function getAllServicePrices(
+    servicePrice1,
+    servicePrice2
+) {
+    return servicePrice1 + servicePrice2;
+};
+servicePercentPrice = function getServicePercentPrices(fullPrice, rollback) {
+    return fullPrice - rollback;
+};
+
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
+console.log(getRollbackMessage());
+console.log(servicePercentPrice);
+console.log(screens.split(","));
