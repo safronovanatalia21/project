50df187f2cae961f3a28e09c512a545e087dccb7
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-10 19:37:51 +0300

    Доработка дз к уроку4

--- script.js
+++ script.js
@@ -9,51 +9,55 @@ let service1 = prompt("Какой дополнительный тип услуг
 let servicePrice1 = prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = prompt("Сколько это будет стоить?");
+let allServicePrices = 0.0;
 let servicePercentPrice = Math.ceil(fullPrice - rollback);
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-switch (fullPrice) {
-    case fullPrice > 30000:
-        console.log("Даем скидку в 10%");
-        break;
-    case fullPrice <= 30000 && fullPrice >= 15000:
-        console.log("Даем скидку в 5%");
-        break;
-    case fullPrice < 15000 && fullPrice >= 0:
-        console.log("Скидка не предусмотрена");
-        break;
-    case fullPrice < 0:
-        console.log("Что-то пошло не так");
-        break;
+function getAllServicePrices(servicePrice1, servicePrice2) {
+    return servicePrice1 + servicePrice2;
 }
 
 function getFullPrice(screenPrice, allServicePrices) {
-    fullPrice = screenPrice + allServicePrices;
-    return fullPrice;
+    switch (fullPrice) {
+        case fullPrice > 30000:
+            console.log("Даем скидку в 10%");
+            break;
+        case fullPrice <= 30000 && fullPrice >= 15000:
+            console.log("Даем скидку в 5%");
+            break;
+        case fullPrice < 15000 && fullPrice >= 0:
+            console.log("Скидка не предусмотрена");
+            break;
+        case fullPrice < 0:
+            console.log("Что-то пошло не так");
+            break;
+    }
+    return screenPrice + allServicePrices;
 }
 
 function getTitle(title) {
-    newTitle = title.trim().charAt(0).toUpperCase();
-    return newTitle;
+    return title.trim().charAt(0).toUpperCase();
 }
 
 function getRollbackMessage() {
     return rollback;
 }
 
-const allServicePrices = function getAllServicePrices(
-    servicePrice1,
-    servicePrice2
-) {
-    return servicePrice1 + servicePrice2;
-};
-servicePercentPrice = function getServicePercentPrices(fullPrice, rollback) {
+function getServicePercentPrices(fullPrice, rollback) {
     return fullPrice - rollback;
-};
+}
 
+allServicePrices = getFullPrice(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+newTitle = getTitle(title);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+console.log(allServicePrices);
+console.log(fullPrice);
+console.log(title);
+console.log(servicePercentPrice);
+console.log(screens.split(","));
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(getRollbackMessage());
 console.log(servicePercentPrice);
-console.log(screens.split(","));
