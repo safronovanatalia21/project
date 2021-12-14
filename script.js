b03b0116205e79d2844142e18af491fc294bfc9e
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-14 23:59:24 +0300

    Доработка к уроку 05

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let title;
-let newTitle;
 let screens;
 let screenPrice;
 let input;
 let num;
 let fullPrice;
-let rollback = (5 * fullPrice) / 100;
+let rollback = 10;
 let adaptive;
 let service1;
 let servicePrice1;
 let service2;
 let servicePrice2;
 let allServicePrices;
-let servicePercentPrice = Math.ceil(fullPrice - rollback);
+let servicePercentPrice;
 
 function isNumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
@@ -25,71 +24,68 @@ function userInput() {
         "Для планшетов, для телефонов"
     );
     do {
-        screenPrice = +prompt("Сколько будет стоить данная работа?");
+        screenPrice = prompt("Сколько будет стоить данная работа?");
     } while (!isNumber(screenPrice));
-
-    service1 = prompt("Нужны ли дополнительные услуги?");
-
-    while (!isNumber(servicePrice1)) {
-        servicePrice1 = prompt("Сколько это будет стоить?");
-    }
-
-    service2 = prompt("Нужны ли дополнительные услуги?");
-
-    while (!isNumber(servicePrice2)) {
-        servicePrice2 = prompt("Сколько это будет стоить?");
-    }
 
     adaptive = confirm("Нужен ли адаптив на сайте?");
 }
+const getAllServicePrices = function() {
+    let sum = 0;
 
-function getAllServicePrices(servicePrice1, servicePrice2) {
-    return servicePrice1 + servicePrice2;
-}
+    for (let i = 0; i < 2; i++) {
+        let price = 0;
 
-function getFullPrice(screenPrice, allServicePrices) {
-    return screenPrice + allServicePrices;
-}
+        if (i === 0) {
+            service1 = prompt("Нужны ли дополнительные услуги?");
+        } else if (i === 1) {
+            service2 = prompt("Нужны ли дополнительные услуги?");
+        }
+        do {
+            price = prompt("Сколько это будет стоить?");
+        } while (!isNumber(price));
 
-function getTitle(title) {
-    return title.trim().charAt(0).toUpperCase();
-}
+        sum += +price;
+    }
 
-function getRollbackMessage() {
-    return rollback;
-}
+    return sum;
+};
 
-function getServicePercentPrices(fullPrice, rollback) {
-    return fullPrice - rollback;
-}
+const getFullPrice = function() {
+    return +screenPrice + allServicePrices;
+};
 
-switch (fullPrice) {
-    case fullPrice > 30000:
-        console.log("Даем скидку в 10%");
-        break;
-    case fullPrice <= 30000 && fullPrice >= 15000:
-        console.log("Даем скидку в 5%");
-        break;
-    case fullPrice < 15000 && fullPrice >= 0:
-        console.log("Скидка не предусмотрена");
-        break;
-    case fullPrice < 0:
-        console.log("Что-то пошло не так");
-        break;
-}
+const getServicePercentPrices = function() {
+    return fullPrice - fullPrice * (rollback / 100);
+};
 
-screenPrice = Number(getFullPrice());
-allServicePrices = Number(getFullPrice(servicePrice1, servicePrice2));
-fullPrice = Number(getFullPrice(screenPrice, allServicePrices));
-servicePercentPrice = Number(getServicePercentPrices(fullPrice, rollback));
+const getTitle = function() {
+    return (
+        title.trim().charAt(0).toUpperCase() + title.trim().substr(1).toLowerCase()
+    );
+};
 
-console.log(isNumber(screenPrice));
-console.log(isNumber(allServicePrices));
-console.log(isNumber(fullPrice));
-console.log(isNumber(title));
-console.log(isNumber(servicePercentPrice));
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        return "Даем скидку в 10%";
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%";
+    } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена";
+    } else if (price < 0) {
+        return "Что-то пошло не так";
+    }
+};
+
+userInput();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+console.log("allServicePrices", allServicePrices);
+console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-console.log(getRollbackMessage());
+console.log(screens.length);
 console.log(servicePercentPrice);
