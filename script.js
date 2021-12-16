4ed3db43e8708c0e4e1596a5c9a97effa95cd4db
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-16 23:15:12 +0300

    Доработка домашнего задания 2 к уроку 7

--- script.js
+++ script.js
@@ -9,80 +9,78 @@ const appData = {
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
+    allServicePrice: 0,
+    userInput: function() {
+        appData.title = prompt("Как называется ваш проект", "Пункт выдачи заказов");
+        appData.screens = prompt(
+            "Какие типы экранов нужно разработать?",
+            "Для планшетов, для телефонов"
+        );
+        do {
+            appData.screenPrice = prompt("Сколько будет стоить данная работа?");
+        } while (!appData.isNumber(appData.screenPrice));
+
+        appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+    },
+    getServicePercentPrices: function() {
+        return appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+    },
+    getAllServicePrices: function() {
+        let sum = 0;
+
+        for (let i = 0; i < 2; i++) {
+            let price = 0;
+
+            if (i === 0) {
+                appData.service1 = prompt("Нужны ли дополнительные услуги?");
+            } else if (i === 1) {
+                appData.service2 = prompt("Нужны ли дополнительные услуги?");
+            }
+            do {
+                price = prompt("Сколько это будет стоить?");
+            } while (!appData.isNumber(price));
+
+            sum += +price;
+        }
+
+        return sum;
+    },
+    getFullPrice: function() {
+        return +appData.screenPrice + appData.allServicePrices;
+    },
+    getTitle: function() {
+        return (
+            appData.title.trim().charAt(0).toUpperCase() +
+            appData.title.trim().substr(1).toLowerCase()
+        );
+    },
+    getRollbackMessage: function(price) {
+        if (price >= 30000) {
+            return "Даем скидку в 10%";
+        } else if (price >= 15000 && price < 30000) {
+            return "Даем скидку в 5%";
+        } else if (price >= 0 && price < 15000) {
+            return "Скидка не предусмотрена";
+        } else if (price < 0) {
+            return "Что-то пошло не так";
+        }
+    },
+    isNumber: function(num) {
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    },
     logger: () => {
         console.log("Уровень логгирования: ИНФО");
-        userInput();
-        getFullPrice();
-        getTitle();
-        getAllServicePrices();
     },
 };
 
-function isNumber(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-}
-
-function userInput() {
-    title = prompt("Как называется ваш проект", "Пункт выдачи заказов");
-    screens = prompt(
-        "Какие типы экранов нужно разработать?",
-        "Для планшетов, для телефонов"
-    );
-    do {
-        screenPrice = prompt("Сколько будет стоить данная работа?");
-    } while (!isNumber(screenPrice));
-
-    adaptive = confirm("Нужен ли адаптив на сайте?");
-}
-const getAllServicePrices = function() {
-    let sum = 0;
-
-    for (let i = 0; i < 2; i++) {
-        let price = 0;
-
-        if (i === 0) {
-            service1 = prompt("Нужны ли дополнительные услуги?");
-        } else if (i === 1) {
-            service2 = prompt("Нужны ли дополнительные услуги?");
-        }
-        do {
-            price = prompt("Сколько это будет стоить?");
-        } while (!isNumber(price));
-
-        sum += +price;
-    }
-
-    return sum;
-};
-
-const getFullPrice = function() {
-    return +screenPrice + allServicePrices;
-};
-
-const getServicePercentPrices = function() {
-    return fullPrice - fullPrice * (rollback / 100);
-};
-
-const getTitle = function() {
-    return (
-        title.trim().charAt(0).toUpperCase() + title.trim().substr(1).toLowerCase()
-    );
-};
-
-const getRollbackMessage = function(price) {
-    if (price >= 30000) {
-        return "Даем скидку в 10%";
-    } else if (price >= 15000 && price < 30000) {
-        return "Даем скидку в 5%";
-    } else if (price >= 0 && price < 15000) {
-        return "Скидка не предусмотрена";
-    } else if (price < 0) {
-        return "Что-то пошло не так";
-    }
-};
-
-function start() {
+const start = function() {
+    appData.userInput();
+    appData.isNumber();
+    appData.getAllServicePrices();
+    appData.getFullPrice();
+    appData.getTitle();
+    appData.getRollbackMessage();
     appData.logger();
-}
-allServicePrices = getAllServicePrices();
+};
+
 start();
