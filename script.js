b6e443a3a6684c5523793734794ce7f340923880
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-19 00:44:59 +0300

    Домашнее задание к уроку 8

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
 const appData = {
     title: "",
-    screens: "",
+    screens: [],
     screenPrice: 0,
     adaptive: true,
-    service1: "",
-    service2: "",
+    services: {},
     rollback: 10,
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
     allServicePrice: 0,
     userInput: function() {
-        appData.title = prompt("Как называется ваш проект", "Пункт выдачи заказов");
-        appData.screens = prompt(
-            "Какие типы экранов нужно разработать?",
-            "Для планшетов, для телефонов"
-        );
         do {
-            appData.screenPrice = prompt("Сколько будет стоить данная работа?");
-        } while (!appData.isNumber(appData.screenPrice));
-
-        appData.adaptive = confirm("Нужен ли адаптив на сайте?");
-    },
-    getServicePercentPrices: function() {
-        return appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
-    },
-    getAllServicePrices: function() {
-        let sum = 0;
+            appData.title = prompt(
+                "Как называется ваш проект",
+                "Пункт выдачи заказов"
+            );
+        } while (!(typeof appData.title === "string"));
 
         for (let i = 0; i < 2; i++) {
+            let name;
+            do {
+                name = prompt("Какие типы экранов нужно разработать?");
+            } while (!(typeof name === "string"));
             let price = 0;
 
-            if (i === 0) {
-                appData.service1 = prompt("Нужны ли дополнительные услуги?");
-            } else if (i === 1) {
-                appData.service2 = prompt("Нужны ли дополнительные услуги?");
-            }
+            do {
+                price = prompt("Сколько будет стоить данная работа?");
+            } while (!appData.isNumber(price));
+
+            appData.screens.push({ id: i, name: name, price: price });
+        }
+
+        for (let i = 0; i < 2; i++) {
+            let name;
+            do {
+                name = prompt("Какой дополнительный тип услуги нужен?");
+            } while (!(typeof name === "string"));
+            let price = 0;
             do {
                 price = prompt("Сколько это будет стоить?");
             } while (!appData.isNumber(price));
-
-            sum += +price;
+            appData.services[name] = +price;
         }
-
-        return sum;
+        appData.adaptive = confirm("Нужен ли адаптив на сайте?");
     },
+
+    addPrice: function() {
+        for (let screen of appData.screens) {
+            appData.screenPrice += +screen.price;
+        }
+        for (let key in appData.services) {
+            appData.allServicePrices += appData.services[key];
+        }
+    },
+
+    getServicePercentPrices: function() {
+        appData.servicePercentPrice =
+            appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+    },
+
     getFullPrice: function() {
-        return +appData.screenPrice + appData.allServicePrices;
+        appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
     },
     getTitle: function() {
-        return (
+        appData.title =
             appData.title.trim().charAt(0).toUpperCase() +
-            appData.title.trim().substr(1).toLowerCase()
-        );
+            appData.title.trim().substr(1).toLowerCase();
     },
     getRollbackMessage: function(price) {
         if (price >= 30000) {
@@ -69,13 +82,16 @@ const appData = {
         return !isNaN(parseFloat(num)) && isFinite(num);
     },
     logger: () => {
-        console.log("Уровень логгирования: ИНФО");
+        console.log(appData.fullPrice);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens);
     },
     start: () => {
         appData.userInput();
+        appData.addPrice();
         appData.isNumber();
-        appData.getAllServicePrices();
         appData.getFullPrice();
+        appData.getServicePercentPrices();
         appData.getTitle();
         appData.getRollbackMessage();
         appData.logger();
